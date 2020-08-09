bbe170e923fb4a7d2abfed9749547010dde99944
Nihal Mansour <nihalmansour0599@gmail.com>
2020-08-09 19:14:29 +0200

    About page

--- src/components/Aboutpage.js
+++ src/components/Aboutpage.js
@@ -1,9 +1,19 @@
 import React from 'react'
+import './Aboutpage.css'
 
 export default function Aboutpage() {
     return (
-        <div>
-            <h1>About Page</h1>
+        <div className="containerbox">
+            <div className="titleabout">
+                <h3 className="about">About</h3>
+            </div>   
+            <hr className="hrline"/>
+            <div className="aboutcontent">
+            <p className="aboutmytodolist">
+                This To-do-List allows you to organize your time and write down your tasks, and if a task
+                finished you can mark it as done and you can also delete it.
+            </p>
+            </div>
         </div>
     )
 }
